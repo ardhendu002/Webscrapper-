0865e9ea83df6f6160897e64216909ad186d9f11
ardhendu002 <ardhendupurnendu@gmail.com>
2026-04-02 23:33:54 +0530

    working fetched

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
 import axios from 'axios';
-import * as cheerio  from 'cheerio';
-const url = 'https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/stats'
+import * as cheerio from 'cheerio';
+
+const url = 'https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/stats';
+
 const AxiosInstance = axios.create({});
 
-interface Playerata{
-    name: String;
-    matches: Number;
-    runs: Number;
+interface PlayerData {
+  name: string;
+  matches: number;
+  runs: number;
 }
+
 AxiosInstance.get(url)
-    .then(
-        response => {
-            const html = response.data as string;
-            const $ = cheerio.load(html);
-            const statsTable : cheerio.Cheerio = $('.statsTableContainer > tr');
-            const topScorers : Playerata[] = [];
-            statsTable.each((i,elem)) => {
-                 
-            }
-        }
-    )
-    .catch(console.error);
+  .then(response => {
+    const html = response.data as string;
+    const $ = cheerio.load(html);
+    const statsTable: cheerio.Cheerio<any> = $('.cb-series-stats-container tr');
+    const topScorers: PlayerData[] = [];
+
+    statsTable.each((i, elem) => {
+      const name: string = $(elem).find('.cb-col-50').text().trim();
+      const matches: number = parseInt($(elem).find('.cb-col-8').eq(0).text().trim());
+      const runs: number = parseInt($(elem).find('.cb-col-8').eq(1).text().trim());
+
+      // Skip empty rows
+      if (name) {
+        topScorers.push({ name, matches, runs });
+      }
+    });
+
+    console.log(topScorers);
+  })
+  .catch(console.error);
